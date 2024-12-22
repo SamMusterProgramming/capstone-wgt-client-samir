b2ea3b96aeceabb2bb8f645f0a2a7d2163875541
Samir Haddadi <sam20232022@gmail.com>
2024-12-22 04:53:39 +0000

    screen

--- src/root/pages/Home.jsx
+++ src/root/pages/Home.jsx
@@ -6,13 +6,13 @@ const Home = ({user}) => {
   return (
      <div className="d-flex flex-column align-items-center  justify-content-center" 
      style={{maxWidth:'500px',minHeight:'100%',width:'100%',height:'85vh'}}>   
-            <div className="d-flex  mt-1"
+            {/* <div className="d-flex  mt-1"
                style={{minWidth:'350px', maxWidth:'10%'}}>     
                <div class="fancy-welcome">
                   <h1>Welcome to tyour Home page!</h1>
                   <p>Make your selection down Below </p>
                </div>  
-            </div> 
+            </div>  */}
            
          
             <div  className ="d-flex justify-content-center mt-3 gap-3 flex-wrap selectcontainer"> 
